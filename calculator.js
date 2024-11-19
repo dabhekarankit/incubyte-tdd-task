709947e88d9dbbf5936c25017b07e1ad3c146490
Ankit Dabhekar <dabhekarankit4.dev@gmail.com>
2024-11-20 00:30:15 +0530

    feat: checking if variable have only number values

--- calculator.js
+++ calculator.js
@@ -1,6 +1,15 @@
 function Add(numbers) {
     // check if variable empty or blank string return 0
     if (!numbers) return 0;
+
+    if (typeof numbers === "number") {
+        return numbers;
+    }
+
+    // check if variable have only number value
+    if (typeof numbers === "string" && /^[0-9]+$/.test(numbers)) {
+        return Number(numbers);
+    }
 }
 
 export { Add };
--- test/calculator.test.js
+++ test/calculator.test.js
@@ -6,4 +6,11 @@ describe("String Calculator", () => {
     it("should return 0 for an empty string", () => {
         expect(Add("")).to.equal(0);
     });
+
+    // If there is a single number
+    it("should return the number itself for a single number", () => {
+        expect(Add("1")).to.equal(1);
+        expect(Add(2)).to.equal(2);
+        expect(Add("5")).to.equal(5);
+    });
 });
